dc988e030fdb0b4f77bb031ab637884f1626a24d
xnithunx <nithun.nanda@gmail.com>
2018-10-09 16:46:54 +0800

    created initial DB

---
+++ project-2/tables.sql
@@ -0,0 +1,49 @@
+
+DROP TABLE IF EXISTS donors;
+DROP TABLE IF EXISTS orgz;
+DROP TABLE IF EXISTS donors_orgz;
+DROP TABLE IF EXISTS donation_list;
+
+
+CREATE TABLE IF NOT EXISTS donors (
+		id SERIAL PRIMARY KEY,
+		donor_name TEXT,
+		donor_email TEXT,
+		donor_location TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS orgz (
+		id SERIAL PRIMARY KEY,
+		orgz_name TEXT,
+		orgz_location TEXT,
+		orgz_about TEXT,
+		orgz_impact_score TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS donors_orgz (
+		id SERIAL PRIMARY KEY,
+		donors_name TEXT,
+		orgz_name TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS donation_list (
+		id SERIAL PRIMARY KEY,
+		donors_name TEXT,
+		orgz_name TEXT,
+		donated INTEGER
+);
+
+
+
+
+
+
+
+
+
+
+
+
