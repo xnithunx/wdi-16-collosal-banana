6d2d61accc1e9ae0ff795742301a65f4a8748882
xnithunx <nithun.nanda@gmail.com>
2018-10-10 20:22:33 +0800

    updated 10 Oct

---
+++ project-2/donors.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO donors (donor_name , donor_email, donor_location, donor_password) VALUES('john', 'john@gmail.com', 'Singapore', 'john');
+
+INSERT INTO donors (donor_name , donor_email, donor_location, donor_password) VALUES('ram', 'ram@gmail.com', 'Australia', 'ram');
+
+INSERT INTO donors (donor_name , donor_email, donor_location, donor_password) VALUES('lee', 'lee@gmail.com', 'Thailand', 'lee');
