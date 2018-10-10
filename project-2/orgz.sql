608db4b6439d60982c7348c28e8e1e333ea78bb3
xnithunx <nithun.nanda@gmail.com>
2018-10-10 20:23:30 +0800

    updated 10 Oct

---
+++ project-2/orgz.sql
@@ -0,0 +1,12 @@
+INSERT INTO orgz (orgz_name, orgz_location, orgz_about, orgz_impact_score) VALUES('Sisterhood Agenda, Inc.','Virgin Islands', 'a Sisterhood Agenda is an award-winning nonprofit organization that creates and
+implements sisterhood activities for women and girls around the globe.
+The organization promotes positive social change and has over 3,500 global partners in 34 countries', '8');
+
+
+INSERT INTO orgz (orgz_name, orgz_location, orgz_about, orgz_impact_score) VALUES('ECCA Camps', 'Nepal', 'Practical learning of environmental parameters through hands-on involvement during course work is a challenge in today curriculum in Nepal. Hence, this project help student to understand and learn various environmental issues and their effect in our daily life through hands-on involvement.', '8');
+
+
+
+INSERT INTO orgz (orgz_name, orgz_location, orgz_about, orgz_impact_score) VALUES('Green Maps','USA', 'Green Maps inspire people to take action, so every community can be a healthier and more sustainable place! In more than 900 cities in 65 countries, locally-made Green Maps directly engage all ages by sharing important information about farmers markets, solar sites, cycling paths, community gardens, green job training centers, and much more.', '7');
+
+
